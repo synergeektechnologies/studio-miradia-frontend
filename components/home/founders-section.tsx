1ca9d282b42e03842bb0553958eed6121007c82f
Synergeek Technologies <synergeektechnologies@gmail.com>
2025-10-21 19:52:59 +0530

    updated founders section mobile responsiveness 3

--- components/home/founders-section.tsx
+++ components/home/founders-section.tsx
@@ -2,24 +2,11 @@ export function FoundersSection() {
   return (
     <section className="relative w-full">
       <div
-        className="w-full h-[50vh] sm:h-[60vh] md:h-[70vh] lg:h-[90vh]"
+        className="w-full h-[50vh] sm:h-[60vh] md:h-[70vh] lg:h-[90vh] bg-cover bg-center bg-no-repeat bg-scroll md:bg-fixed"
         style={{
           backgroundImage: "url('/Untitled-9.jpg')",
-          backgroundAttachment: "scroll", // Mobile: scroll normally
-          backgroundPosition: "center top",
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
         }}
-        // Desktop: fixed background (parallax effect)
-        data-mobile-scroll="true"
       />
-      <style jsx>{`
-        @media (min-width: 768px) {
-          div[data-mobile-scroll="true"] {
-            background-attachment: fixed !important;
-          }
-        }
-      `}</style>
     </section>
   )
 }
