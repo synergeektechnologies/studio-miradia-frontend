2616dc916e058303ef8d057575ce3f075d7408b5
Synergeek Technologies <synergeektechnologies@gmail.com>
2025-10-22 09:40:53 +0530

    fixed parallax on ios devices

--- components/home/founders-section.tsx
+++ components/home/founders-section.tsx
@@ -1,4 +1,10 @@
+"use client"
+
+import { useIsIOS } from "../../hooks/use-ios"
+
 export function FoundersSection() {
+  const isIOS = useIsIOS()
+
   return (
     <section className="relative">
       {/* Parallax Background Image */}
@@ -6,10 +12,12 @@ export function FoundersSection() {
         className="relative min-h-[600px] md:min-h-[700px] flex items-center justify-center"
         style={{
           backgroundImage: "url('/Untitled-9.jpg')",
-          backgroundAttachment: "fixed",
-          backgroundPosition: "top",
+          backgroundAttachment: isIOS ? "scroll" : "fixed",
+          backgroundPosition: isIOS ? "center top" : "top",
           backgroundRepeat: "no-repeat",
           backgroundSize: "cover",
+          // Add will-change for better performance on iOS
+          willChange: isIOS ? "auto" : "transform",
         }}
       />
     </section>
